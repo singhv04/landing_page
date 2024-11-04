b9f14c850553cb7239b068c3c44873957a888461
Shakib-Shazzadul <shakib1186@gmail.com>
2024-11-04 19:25:35 +0600

    reduce space in hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,12 +1,13 @@
 const Hero: React.FC = () => {
   return (
-    <div className="cg relative mx-auto w-[90%] text-center text-foreground lg:w-[80%]">
+    <div className=" relative mx-auto w-[90%] text-center text-foreground lg:w-[80%]">
       <h1 className="text-xl font-medium leading-custom text-emerald-500 md:text-3xl md:leading-custom">
         Transform Conversations, Scale Effortlessly with Custom AI Caller.{" "}
       </h1>
       <div className="my-8 px-6 text-xs  md:text-base lg:px-[200px] text-balance">
         <p>
-        Boost your business with smart voice automation that enhances customer satisfaction and simplifies operations.
+          Boost your business with smart voice automation that enhances customer
+          satisfaction and simplifies operations.
         </p>
       </div>
     </div>
