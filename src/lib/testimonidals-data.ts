05d49453bbf0384ec6d96e2089150569d5c06a11
singhv04 <vaibhavsinghvr007@gmail.com>
2024-11-06 17:57:20 +0530

    updated testimonials

--- src/lib/testimonidals-data.ts
+++ src/lib/testimonidals-data.ts
@@ -2,36 +2,107 @@ import { Testimonial } from "@/types/all-types";
 
 export const testimonials: Testimonial[] = [
   {
+    name: "Gary Lee",
+    position: "CEO of BuyCondo, Singapore",
+    text: "The changes Minecraft made to our website and the integration of the AI chatbot have revolutionized the way we do business. Our clients appretiate how fast they can get answers, schedule viewings, and find properties. The automation has taken a significant load off our team, alowing us to focus on building relationships and closing deals. Our lead conversion rate has noticeably improved, and we are exited to continue growing with these new tools.",
+  },
+  
+  {
     name: "Shashank Shekhar",
     position: "Product manager, Wego",
     text: "Collaborating with this AI agency for chatbot integration was a strategic move. We observed a 20% growth in lead conversions, and the personalized user experiences contributed to a 25% boost in customer retention.",
     image: "https://github.com/shadcn.png",
   },
+  
   {
     name: "Sushant Mehta",
     position: "Co-Founder,Wiser",
     text: "The precision and intelligence embedded in the chatbot solutions were truly commendable. Noteworthy was the 15% increase in first-contact query resolution, reflecting the AI agency's commitment to excellence.",
-    image: "https://github.com/shadcn.png",
   },
+  
   {
     name: "Neelabh Singh",
     position: "Co-Founder, Lushful",
     text: "Incorporating automation solutions from this AI agency was transformative. They not only elevated our workflow efficiency, realizing a 20% drop in manual errors, but also imparted invaluable insights into AI's role, contributing to a significant 15% boost in overall productivity. Their expertise and guidance have been instrumental in our success.",
-    image: "https://github.com/shadcn.png",
   },
-  {
-    name: "Pranav Gupta",
-    position: "Founder, Edsplore",
-    text: "The strategic implementation of chatbots by this AI agency significantly amplified our customer interaction metrics. We observed a 30% increase in customer engagement and a notable improvement in query resolution time",
-  },
+
   {
     name: "Tiago Pereira",
-    position: "Founding Member",
+    position: "Founding Member,Quinta de Monserrate",
     text: "Implementing the AI chatbot has been a huge success for us. Not only has it streamlined the booking process across out multiple locations, but it has also improved our ability to engage with customers in a meaningful way. Our stuff are less overwhelmed, our clients are happier, and the insights we gain from the chatbot are invaluable for ourr future growth. Quinta de Monserrate can now expand confidently, knowing that we have a system that grows with us.",
   },
   {
-    name: "Gary Lee",
-    position: "CEO of BuyCondo",
-    text: "The changes Minecraft made to our website and the integration of the AI chatbot have revolutionized the way we do business. Our clients appretiate how fast they can get answers, schedule viewings, and find properties. The automation has taken a significant load off our team, alowing us to focus on building relationships and closing deals. Our lead conversion rate has noticeably improved, and we are exited to continue growing with these new tools.",
+    name: "Ram Chella",
+    position: "CEO, FoodieBot",
+    text: "The AI-powered order-taking system and WebRTC module have completely transformed our platform. The real-time video and voice features have created a more interactive experience for our customers, and the AI phone assistant has made it possible to handle thousands of orders every day without any hiccups. Our restaurants are seeing more orders, fewer mistakes, and happier customers.",
+    image: "/testimonial_imgs/RamChella.jpeg",
   },
+  
+  {
+    name: "jordanstubb",
+    position: "United States",
+    text: "I’ve spent months trying to find somebody that could do a call bot and there’s so many “fake expert”. These guys are the real deal. Their communication is great, they’re very patient and quick to make revisions and they are truly the best in the world at what they do. I’ll be a customer for life and their biggest advocate forever! Thank you so much!",
+    image: "/testimonial_imgs/jordanstubb.webp",
+  },
+
+  {
+    name: "rosamaskco",
+    position: "Australia",
+    text: "They built me a completely custom AI salesman and it sounds super realistic, best one I've had yet. They're the real professionals and made it super simple for me too, especially because it's such a new industry their team was like finding a diamond in the mud! They did the revisions in only a few hours and after one set of revisions the salesman was perfect. Thanks again and I'll be thanks my next job with them soon.",
+    image: "/testimonial_imgs/rosamaskco.webp",
+  },
+
+  {
+    name: "yves56",
+    position: "Canada",
+    text: "Absolutely exceptional experience, the professionalism and dedication to deliver top-notch work is truly commendable. From the moment I shared my project requirements, they provided insightful suggestions significantly enhancing the final product which aligned perfectly with my vision. They were always prompt in his responses, ensuring that I was kept in the loop at every stage of the project, making the entire experience stress-free and efficient.Highly recommended!",
+    image: "/testimonial_imgs/yves56.webp",
+  },
+
+
+  {
+    name: "lisastories",
+    position: "United States",
+    text: "They did an amazing job developing an AI-powered chatbot and caller for my restaurant business. The bots have been running smoothly handling customer inquiries, orders, and reservations seamlessly. They took the time to understand our needs and delivered a solution that’s efficient, reliable, and perfectly suited to our operations. Our customers are happier, and the business has become more streamlined. I couldn’t be more pleased with the results—highly recommend them for anyone looking for top-notch AI solutions!",
+    image: "/testimonial_imgs/lisastories.webp",
+  },
+
+
+
+  {
+    name: "oel king",
+    position: "Germany",
+    text: "After a long search for a reliable provider for, I finally found the right one. The service here is not only professional but also incredibly attentive and adaptable. No matter what requests or changes I had, the team always responded quickly and with great solutions. Their expertise and passion for what they do are truly evident. I feel well taken care of and am confident that I made the best choice here. For me, it's clear: I will stay with them and recommend their work to anyone looking for quality and reliability. Thank you for this outstanding service!",
+  },
+
+
+  {
+    name: "wellbuddy",
+    position: "France",
+    text: "I am very happy of the collaboration with them on my integration project. They was very reactive and immediately understood the problem I was facing in my project. Would recommend their services without hesitating.",
+    image: "/testimonial_imgs/wellbuddy.webp",
+  },
+
+  {
+    name: "jammy1",
+    position: "United States",
+    text: "They exceeded my expectations with top-notch professionalism and impeccable attention to detail in AI development. His language fluency and quick responsiveness made working together a breeze, and he went ABOVE AND BEYOND. Highly recommend! 🙌",
+    image: "/testimonial_imgs/jammy1.webp",
+  },
+
+  {
+    name: "canera",
+    position: "Switzerland",
+    text: "It is such a pleasure working with them, their understanding of bots is incredible, he combines technological understanding with a strong business accumen. they gave me great advises which were always correct, i strongly recommend working with them, when you are thinking about chat-bots with high complexitiy",
+    image: "/testimonial_imgs/canera.webp",
+  },
+
+
+  {
+    name: "vladislav_ripko",
+    position: "Ukraine",
+    text: "Absolutely thrilled with their work! The custom IT solution delivered was not only complex but also incredibly well-executed. They worked with impressive speed throughout the development process, ensuring every detail was meticulously handled. Highly recommend to work with this team!",
+    image: "/testimonial_imgs/vladislav_ripko.webp",
+  },
+
 ];
